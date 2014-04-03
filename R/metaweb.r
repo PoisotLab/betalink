34481bc230ec3496a5373a98875c0e969b5162c9
Timothée Poisot <t.poisot@gmail.com>
2014-04-03 11:25:24 -0400

    Updates in the metaweb function, returns three matrices with adjacency, number of interactions, and number of co-occurences

--- R/metaweb.r
+++ R/metaweb.r
@@ -1,17 +1,26 @@
-metaweb = function(W){
-	Lo = unique(unlist(lapply(W,colnames)))
-	Up = unique(unlist(lapply(W,rownames)))
-	meta = matrix(0,ncol=length(Lo),nrow=length(Up))
-	colnames(meta) = Lo
-	rownames(meta) = Up
-	co.oc = meta
+#' @title Returns a metaweb given a list of networks
+#' @description
+#' Given a list of networks, this function returns the metaweb, and informations related to co-occurrence
+#'
+#' @param W a \code{list} of matrices
+#' @export
+metaweb <- function(W){
+	Lo <- unique(unlist(lapply(W,colnames)))
+	Up <- unique(unlist(lapply(W,rownames)))
+	meta <- matrix(0,ncol=length(Lo),nrow=length(Up))
+	colnames(meta) <- Lo
+	rownames(meta) <- Up
+	cooc <- meta
 	for(w in W){
-		w[w>0] = 1
-		meta[rownames(w),colnames(w)] = meta[rownames(w),colnames(w)] + w
-		co.oc[rownames(w),colnames(w)] = co.oc[rownames(w),colnames(w)] + 1
+		w[w>0] <- 1
+		meta[rownames(w),colnames(w)] <- meta[rownames(w),colnames(w)] + w
+		cooc[rownames(w),colnames(w)] <- cooc[rownames(w),colnames(w)] + 1
 	}
-	null.template = meta/co.oc
-	null.template[is.nan(null.template)] = 0
-	meta[meta>0] = 1
-	return(list(web=meta,template=null.template, cooc = co.oc))
+   adjacency <- meta
+	adjacency[adjacency>0] <- 1
+	return(list(
+               web = adjacency,
+               n_cooc = cooc,
+               n_int = meta
+               ))
 }
