64a3bdbe5a640cec11df37afcebd6aacb5b4775f
Timothée Poisot <t.poisot@gmail.com>
2014-04-08 15:20:26 -0400

    beta_os_prime simplified

--- R/beta_os_prime.R
+++ R/beta_os_prime.R
@@ -2,18 +2,14 @@
 #' @description
 #' Returns the values of beta OS', i.e. the distace between all realizations, and the revelant subset from the metaweb
 #'
-#' @param W a list of networks
+#' @param N a list of networks
 #' @param ... additional arguments to be passed to \link{betalink}
 #'
 #' @return An array of the values of Beta OS'
 #'
 #' @export
-beta_os_prime <- function(W, ...){
-	metaweb <- metaweb(W)$web
-	os_prime <- NULL
-	for(w in W){
-		partitions <- betalink(w,metaweb[rownames(w),colnames(w)],...)
-		os_prime <- c(os_prime,partitions$OS)
-	}
+beta_os_prime <- function(N, ...){
+	M <- metaweb(N)
+	os_prime <- laply(N, function(x) betalink(x, M, ...)$OS)
 	return(os_prime)
 }
