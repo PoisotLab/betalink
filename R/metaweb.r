9fb5d4674a0aaedaebd4599cdfdbf95ebd4a7d89
Timothée Poisot <t.poisot@gmail.com>
2014-04-08 14:48:32 -0400

    New metaweb function with igraph addition

--- R/metaweb.r
+++ R/metaweb.r
@@ -1,26 +1,11 @@
 #' @title Returns a metaweb given a list of networks
 #' @description
-#' Given a list of networks, this function returns the metaweb, and informations related to co-occurrence
+#' Given a list of networks, this function returns the metaweb
 #'
-#' @param W a \code{list} of matrices
+#' @param n a \code{list} of graphs
 #' @export
-metaweb <- function(W){
-	Lo <- unique(unlist(lapply(W,colnames)))
-	Up <- unique(unlist(lapply(W,rownames)))
-	meta <- matrix(0,ncol=length(Lo),nrow=length(Up))
-	colnames(meta) <- Lo
-	rownames(meta) <- Up
-	cooc <- meta
-	for(w in W){
-		w[w>0] <- 1
-		meta[rownames(w),colnames(w)] <- meta[rownames(w),colnames(w)] + w
-		cooc[rownames(w),colnames(w)] <- cooc[rownames(w),colnames(w)] + 1
-	}
-   adjacency <- meta
-	adjacency[adjacency>0] <- 1
-	return(list(
-               web = adjacency,
-               n_cooc = cooc,
-               n_int = meta
-               ))
+metaweb <- function(n){
+   M <- n[[1]]
+   for(i in c(2:length(n))) M <- M + n[[i]]
+   return(M)
 }
