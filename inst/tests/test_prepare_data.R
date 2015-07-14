5ac94c090b02622212b0e7fd4c981b7ff5b6d666
Timothée Poisot <tim@poisotlab.io>
2015-07-14 19:35:08 -0400

    tests

--- inst/tests/test_prepare_data.R
+++ inst/tests/test_prepare_data.R
@@ -1,2 +1,8 @@
 context("Preparing data for analysis")
 
+test_that("It behaves as expected", {
+
+   data(anemonefish)
+   cf <- prepare_networks(anemonefish, TRUE)
+   expect_that(typeof(cf$Fukui), equals("list"))
+})
