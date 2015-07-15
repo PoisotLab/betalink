40ee99d81f9832c7a914b46b31f12a2973b31ca9
Timothée Poisot <tim@poisotlab.io>
2015-07-14 20:42:49 -0400

    Cosmetic changes

--- inst/tests/test_betalink.R
+++ inst/tests/test_betalink.R
@@ -8,7 +8,6 @@ test_that("It behaves as expected", {
    expect_that(betalink(cf[[1]], cf[[1]])$S,  equals(0))
    expect_that(betalink(cf[[2]], cf[[2]])$OS, equals(0))
    expect_that(betalink(cf[[3]], cf[[3]])$WN, equals(0))
-
    expect_that(betalink(cf[[3]], cf[[4]])$WN > 0, is_true())
 
 })
